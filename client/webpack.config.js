fbd9807acdc2001ccbee7ad07e091523bd6e07cb
Ivanildo Pontual Neto <ivanildopontual95@gmail.com>
2018-07-17 11:51:43 -0400

    webpack-scripts

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,16 +1,23 @@
 const path = require('path');
 const extractTextPlugin = require('extract-text-webpack-plugin');
 const optimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const webpack = require('webpack');
 
 let plugins = [];
 
 plugins.push(new extractTextPlugin('styles.css'))
 
-if(process.env.NODE_ENV == 'production'){
+plugins.push(new webpack.ProvidePlugin({
+    '$': 'jquery/dist/jquery.js',
+    'jQuery': 'jquery/dist/jquery.js'
+}));
+
+if (process.env.NODE_ENV == 'production') {
     plugins.push(new optimizeCSSAssetsPlugin({
         cssProcessor: require('cssnano'),
         cssProcessorOptions: {
             discardComments: {
+            //Não remove todos os comentários
                 removeAll: true
             }
         },
